936820c36ed5e2bfe2ca314e356f087c7782dfed
UltrasonicMadness <contact@ultrasonicmadness.org>
2023-03-09 21:47:02 +0000

    Adapt scraper_eastlothian to current version of Beautiful Soup and East Lothian FOI website

--- scraper_eastlothian.py
+++ scraper_eastlothian.py
@@ -2,7 +2,6 @@
 import urllib.request, urllib.error, urllib.parse, re, urllib.parse
 from bs4 import BeautifulSoup
 from datetime import datetime
-from .scraper_aberdeen import im
 
 def scrape():
 	html = urllib.request.urlopen("http://www.eastlothian.gov.uk/site/custom_scripts/foi_download_index.php?currentPage=1&itemsPerPage=2000000").read()
@@ -11,35 +10,33 @@ def scrape():
 	idPattern = r'^http://www.eastlothian.gov.uk/download/downloads/id/(?[0-9]+)/.*$'
 	soup = BeautifulSoup(html)
 
-	rows = soup.table.findAll('tr')
+	rows = soup.main.findAll('li', class_='list__item')
 	reqs = []
 
-	for x in range(1, len(soup.table.findAll('tr'))):
-
-		cols = rows[x].findAll('td')
-		if ( re.search(datePattern, cols[0].string) is not None ):
-			title = re.sub(datePattern, '', cols[0].string).strip()
+	for row in rows:
+		href = row.a.get('href')
+		raw_title = row.a.h3.string
+		if ( re.search(datePattern, raw_title) is not None ):
+			title = re.sub(datePattern, '', raw_title).strip()
 			if title[-1] == '-':
 				title = title[:-1].strip()
-			date = re.search(datePattern, cols[0].string).group(0)
-			iden = urllib.parse.urlparse(cols[0].a['href']).path.split('/')[4]
-			tags = [re.sub(r'^the', '', tag).lower().strip() for tag in cols[3].string.split(" and ")]
-			doc = rows[x].findAll('a')[0].get('href', None)
+			date = re.search(datePattern, raw_title).group(0)
+			iden = urllib.parse.urlparse(href).path.split('/')[4]
+			# tags = [re.sub(r'^the', '', tag).lower().strip() for tag in cols[3].string.split(" and ")]
 
-			tags.append('east lothian')
+			# tags.append('east lothian')
 
 			timestamp = datetime.strptime(date, "%B %Y")
 			reqs.append({'title': title,
-				     'tags': tags,
+				     # 'tags': tags,
 				     'date': timestamp,
 				     'type': 'FOI',
 				     'iden': iden,
-				     'doc': doc})
+				     'doc': href})
 	return reqs
 				
 
 reqs = scrape()
 print('Got ', len(reqs), ' results.')
 print('First: ', reqs[0])
-print('last one:', reqs[-1]) 
-im(reqs, 2)
+print('last one:', reqs[-1])
