4d40d61cb8cc909be50f1a194cfc082b7085ad43
Robert McWilliam <rmcw@allmail.net>
2023-03-18 15:36:11 +0000

    Add translation to dict for serialisation to FOI class and test of that on existing Moray JSON file.

--- foi.py
+++ foi.py
@@ -1,5 +1,5 @@
 # Schema for what we want to gather for each FOI request we are indexing
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from typing import List, Optional
 from datetime import datetime
 
@@ -13,3 +13,11 @@ class FOI:
     tags: List[str] = field(default_factory=list)
     wdtk_id: str  = "" # What Do They Know ID
     body_id: str = "" # ID from the body FOI is sent to  
+
+    def asdict(self):
+        d = asdict(self)
+        if self.last_updated_at:
+            d["last_updated_at"] = self.last_updated_at.isoformat()
+        if self.initial_request_at:
+            d["initial_request_at"] = self.initial_request_at.isoformat()
+        return d
