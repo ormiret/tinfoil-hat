a8ee641d4329a667b3304c040faad894f61402c3
UltrasonicMadness <contact@ultrasonicmadness.org>
2023-03-18 15:34:19 +0000

    Adapt East Lothian scraper to FOI class

--- scraper_eastlothian.py
+++ scraper_eastlothian.py
@@ -1,17 +1,18 @@
-
 import urllib.request, urllib.error, urllib.parse, re, urllib.parse
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+from foi import FOI
 
 def scrape():
 	html = urllib.request.urlopen("http://www.eastlothian.gov.uk/site/custom_scripts/foi_download_index.php?currentPage=1&itemsPerPage=2000000").read()
 
 	datePattern = r'(January|February|March|April|May|June|July|August|September|October|November|December) 20[0-9][0-9]$'
 	idPattern = r'^http://www.eastlothian.gov.uk/download/downloads/id/(?[0-9]+)/.*$'
-	soup = BeautifulSoup(html)
+	soup = BeautifulSoup(html, 'lxml')
 
-	rows = soup.main.findAll('li', class_='list__item')
-	reqs = []
+	rows = soup.main.find_all('li', class_='list__item')
+	foilist = []
 
 	for row in rows:
 		href = row.a.get('href')
@@ -24,19 +25,16 @@ def scrape():
 			iden = urllib.parse.urlparse(href).path.split('/')[4]
 			# tags = [re.sub(r'^the', '', tag).lower().strip() for tag in cols[3].string.split(" and ")]
 
-			# tags.append('east lothian')
+			tags = ['east lothian']
 
 			timestamp = datetime.strptime(date, "%B %Y")
-			reqs.append({'title': title,
-				     # 'tags': tags,
-				     'date': timestamp,
-				     'type': 'FOI',
-				     'iden': iden,
-				     'doc': href})
-	return reqs
+
+			foilist.append(FOI(title=title, last_updated_at=timestamp,
+					  link=href, tags=tags, body_id=iden))
+	return foilist
 				
 
-reqs = scrape()
-print('Got ', len(reqs), ' results.')
-print('First: ', reqs[0])
-print('last one:', reqs[-1])
+foilist = scrape()
+print('Got ', len(foilist), ' results.')
+print('First: ', foilist[0])
+print('last one:', foilist[-1])
