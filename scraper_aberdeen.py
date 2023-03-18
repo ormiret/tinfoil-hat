ee2ef379be58aa4d92de6c77918407fa26d5d5cb
UltrasonicMadness <contact@ultrasonicmadness.org>
2023-03-18 14:51:30 +0000

    Aberdeen scraper now uses FOI class

--- scraper_aberdeen.py
+++ scraper_aberdeen.py
@@ -2,18 +2,20 @@ from bs4 import BeautifulSoup
 import urllib.request, urllib.error, urllib.parse
 from datetime import datetime as dt
 
+from foi import FOI
+
 
 def scrape(year):  # Years between 2018 and 2020 are confirmed to work here
 	html = urllib.request.urlopen('https://www.aberdeencity.gov.uk/services/council-and-democracy/access-information/disclosure-log-' + str(year)).read()
 	soup = BeautifulSoup(html, "lxml")
 
-	reqs = []
+	foilist = []
 	for a in soup.article.find_all('a'):
 		url = urllib.parse.urljoin('https://www.aberdeencity.gov.uk', a.get('href'))
 		if url[0:len("https://www.aberdeencity.gov.uk/")] == "https://www.aberdeencity.gov.uk/":
-			reqs += scrape_category(url, year)
+			foilist += scrape_category(url, year)
 
-	return reqs
+	return foilist
 
 def scrape_category(url, year):
 	html = urllib.request.urlopen(url).read()
@@ -21,27 +23,25 @@ def scrape_category(url, year):
 	tags = soup.title.string.split(':')[1].split('|')[0].strip().lower().split(' and ')
 	tags.append('aberdeen')
 	tags.append('aberdeen city')
-	reqs = []
+	foilist = []
 
 	for x in soup.article.find_all('a'):
-		reqs.append({
-			'doc': x.get('href', None),
-			'title': ' '.join(x.string.split(' ')[1:]).strip("- "),
-			'iden': x.string.split(' ')[0],
-			'type': x.string[0:3],
-			'date': dt.strptime(str(year), '%Y'),
-			'tags': tags
-		})
+		title = ' '.join(x.string.split(' ')[1:]).strip("- ")
+		last_updated_at = dt.strptime(str(year), '%Y')
+		link = x.get('href', None)
+		body_id = x.string.split(' ')[0]
 
-	return reqs
+		foilist.append(FOI(title=title, last_updated_at=last_updated_at,
+					 link=link, tags=tags, body_id=body_id))
+	return foilist
 
 
 if __name__ == "__main__":
 	for year in range(2016, 2021):
-		reqs = scrape(year)
+		foilist = scrape(year)
 		print(f'First of {year}:')
-		print(reqs[0])
+		print(foilist[0])
 		print()
 		print(f'Last of {year}:')
-		print(reqs[-1])
+		print(foilist[-1])
 		print()
