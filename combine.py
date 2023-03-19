6451d67d4d7f34b0ffa5eeedd1d85b57e6d568f6
Robert McWilliam <rmcw@allmail.net>
2023-03-19 10:39:04 +0000

    Add saving first 1000 FOIs to have smaller set for testing.

--- combine.py
+++ combine.py
@@ -11,5 +11,8 @@ full.sort(key=lambda x: x['last_updated_at'], reverse=True)
 with open("json_outputs/full.json", "w") as f:
     json.dump(full, f, indent=2)
 
+with open("json_outputs/first_1000.json", "w") as f:
+    json.dump(full[:1000], f, indent=2)
+
 print(f"Have {len(full)} requests")
 
